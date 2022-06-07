05c3f156324b5ea34cb5c706a3d55ae1853dbc6e
andre-bandeli <andre.lbandeli@gmail.com>
2022-06-07 10:47:45 -0300

    make it better

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, about, home, page_text, page_text_three, page_text_two
+from .views import about, home, page_text, page_text_three, page_text_two
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('', index , name='index'),
-    path('home/', home , name='home'),
+    path('', home , name='home'),
     path('about/', about , name='about'),
     path('text/', page_text , name='page_text'),
     path('text-2/', page_text_two , name='page_text_two'),
--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,8 @@
-from pyexpat import model
+
 from django.shortcuts import render
 
 from .models import ApresentacaoPesquisa, GuiaVideos, Hero, LinksExternos, SlidesPesquisa, SobreMetodologia, SobrePesquisa, SobrePesquisadora, TiposViolencia
 
-
-def index(request):
-
-    data = {}
-    data['apresentacao_pesquisa'] = ApresentacaoPesquisa.objects.all()
-    data['slides_pesquisa'] = SlidesPesquisa.objects.all()
-    data['guia_videos'] = GuiaVideos.objects.all()
-    data['tipos_violencia'] = TiposViolencia.objects.all()
-
-
-    return render(request, 'index.html', data)
 
 
 def home(request):
@@ -28,7 +17,6 @@ def home(request):
 
     return render(request, 'template.html', data)
 
-
 def about(request):
 
     data = {}
@@ -38,6 +26,7 @@ def about(request):
 
 
     return render(request, 'about.html', data)
+
 
 def page_text(request):
     return render(request, 'pagina_texto.html')
